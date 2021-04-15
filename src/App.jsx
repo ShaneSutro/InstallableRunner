d5124fc3b8beec534194a5051e8f611b50198f16
Shane Sutro <shanesutro@gmail.com>
2021-04-15 08:27:26 -0600

    Add css, VB Installable kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from '@vestaboard/installables'
+import './style.css'
 
 class App extends React.Component {
   constructor(props) {
@@ -11,7 +13,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>Configured Correctly Still</h1>
+      <>
+        <h1>Configured Correctly Still</h1>
+        <Button buttonType="default">A Button</Button>
+      </>
     )
   }
 }
